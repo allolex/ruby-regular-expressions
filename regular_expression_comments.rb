5733553d6059bbac3617a9303ac12d9d3df23d92
Damon Davison <damon@allolex.net>
2014-05-02 18:16:02 -0400

    Expand comments example

--- regular_expression_comments.rb
+++ regular_expression_comments.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # Commenting regular expressions
+# Use the x flag after your regular expression
 
 re = /
   \A        # the beginning of a string,
@@ -8,18 +9,53 @@ re = /
   >         # followed by a '>'
   /x
 
-test_text = <<EOS
-<this-one-matches>
- <this-one-does-not-match>
+# Second method for compiling the regex
+# I'm using a heredoc here with `<<`
+re_two_string = <<-'END_OF_REGEX'
+  \A    # beginning of a string
+  \s+   # one or more spaces
+  <     # a '<' character
+  END_OF_REGEX
+
+# Regexp::EXTENDED is the same as the `x` flag
+re_two = Regexp.new(re_two_string, Regexp::EXTENDED)
+
+
+test_text = <<'END_OF_STRING'
+<this-one-matches-the-first-re>
+ <this-one-matches-the-second-re>
 <match>
 <> <!-- no match -->
-EOS
+END_OF_STRING
 
 test_text.split(/\n/).each do |line|
-  message = if !! line.match(re)
-    'SUCH MATCH!'
-  else
-    'MATCH FAIL :('
-  end
-  puts "#{message} '#{line}'"
+  message = if re === line
+              'SUCH MATCH on the first RE !'
+            elsif re_two === line
+              'SUCH MATCH on the second RE !'
+            else
+              'MATCH FAIL :('
+            end
+  puts "#{message} --> '#{line}'"
 end
+
+puts '',re.inspect
+puts '',re_two.inspect
+
+__END__
+SUCH MATCH on the first RE ! --> '<this-one-matches-the-first-re>'
+SUCH MATCH on the second RE ! --> ' <this-one-matches-the-second-re>'
+SUCH MATCH on the first RE ! --> '<match>'
+MATCH FAIL :( --> '<> <!-- no match -->'
+
+/
+  \A        # the beginning of a string,
+  <         # followed by a '<'
+  [^>]+     # followed by one or more non-'>' characters
+  >         # followed by a '>'
+  /x
+
+/  \A    # beginning of a string
+  \s+   # one or more spaces
+  <     # a '<' character
+/x
